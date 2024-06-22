adede880775aa662b633efadc14d14282c90be8e
hikettei <hikettei@gmail.com>
2024-06-22 14:58:05 +0900

    new conds

--- tinygrad/function.py
+++ tinygrad/function.py
@@ -285,8 +285,8 @@ def _xsin(x: LazyBuffer, is_metal: bool=False, fast: bool=False) -> LazyBuffer:
 class Sin(Function):
   def forward(self, x: LazyBuffer, fast:bool=False) -> LazyBuffer:
     self.x = x
-    self.fast = fast or self.device in ["PTX", "NV", "CUDA"]
-    self.fast_approx = x.dtype in [dtypes.float16, dtypes.float32, dtypes.float64]
+    self.fast = fast or self.device in ["AMD", "PTX", "NV", "CUDA"]
+    self.fast_approx = x.dtype in [dtypes.float32, dtypes.float64]
     if self.fast_approx:
       return _xsin(x, is_metal=self.device=="METAL", fast=self.fast)
 
