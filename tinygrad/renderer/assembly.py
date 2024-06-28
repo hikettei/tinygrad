3e6f81ce3814d5a401ea523717a3027675002a0d
hikettei <hikettei@gmail.com>
2024-06-28 15:47:54 +0900

    test

--- tinygrad/renderer/assembly.py
+++ tinygrad/renderer/assembly.py
@@ -163,9 +163,8 @@ class PTXRenderer(Renderer):
             kk(self.asm_for_op[args](ssa("alu", u), *[r[x] for x in src], src[0].dtype, self.types[src[0].dtype]))
           elif args is BinaryOps.SHL or args is BinaryOps.SHR:
             # the right operand must be uint32 regardless of the instruction type
-            assert False
             operands = [r[x] for x in src]
-            operands[1] = _cast(operands[1], dtype, dtypes.uint32, u=u)
+            operands[2] = _cast(operands[2], dtype, dtypes.uint32, u=u)
             kk(self.asm_for_op[args](ssa("alu", u), *operands, src[0].dtype, self.types[src[0].dtype]))
           else:
             kk(self.asm_for_op[args](ssa("alu", u), *[r[x] for x in src], dtype, self.types[dtype]))
